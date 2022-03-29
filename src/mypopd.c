741d410362bfe62922852ff7bd60276d2345d8c6
smethven <smethven@student.ubc.ca>
2022-03-28 21:55:21 -0700

    pop3 user and welcome tests pass

--- src/mypopd.c
+++ src/mypopd.c
@@ -8,6 +8,9 @@
 #include <ctype.h>
 
 #define MAX_LINE_LENGTH 1024
+#define WELCOME_SENT_STATE 0
+#define USER_SENT_STATE 1
+#define PASS_SENT_STATE 2
 
 static void handle_client(int fd);
 
@@ -27,6 +30,7 @@ void handle_client(int fd) {
   
     char recvbuf[MAX_LINE_LENGTH + 1];
     net_buffer_t nb = nb_create(fd, MAX_LINE_LENGTH);
+    int state;
   
     /* TO BE COMPLETED BY THE STUDENT */
 
@@ -41,31 +45,37 @@ void handle_client(int fd) {
     // send greeting message
     if (send_formatted(fd, "+OK POP3 server ready \r\n") != -1){
         dlog("Welcome message sent sucessfully \n");
+        state = WELCOME_SENT_STATE;
     } else {
         dlog("Welcome message sent unsuccessfully \n");
     }
 
     // read line from socket
-    char *parts[MAX_LINE_LENGTH+1];
     int read = nb_read_line(nb, recvbuf);
-    if (read != -1){
+    while ((read != -1) && (read != 0)){
+        char *parts[MAX_LINE_LENGTH+1];
         split(recvbuf, parts);
-        int size = sizeof parts / sizeof parts[0];
-        if (size > 2){
-            send_formatted(fd, "-ERR invalid arguments \r\n");
-        } else if (strcmp(parts[0], "USER") == 0){
-            // check if name in users.txt
-            if (is_valid_user(parts[1], NULL) != 0){
-                // username exists
-                send_formatted(fd, "+OK %s is a valid mailbox \r\n", parts[1]);
-            } else {
-                // username not valid
-                send_formatted(fd, "-ERR no mailbox for %s \r\n", parts[1]);
-            }
-        } else {
-            // client did not send USER message
-            send_formatted(fd, "-ERR \r\n");
+        switch(state){
+            case 0:
+                if (strcmp(parts[0], "USER") == 0){
+                    if ((parts[2] != NULL) || (parts[1] == NULL)){
+                        // wrong number of arguments
+                        send_formatted(fd, "-ERR invalid arguments \r\n");
+                    } else if (is_valid_user(parts[1], NULL) != 0){
+                        // username exists
+                        send_formatted(fd, "+OK %s is a valid mailbox \r\n", parts[1]);
+                        state = USER_SENT_STATE;
+                    } else {
+                        // username not valid
+                        send_formatted(fd, "-ERR no mailbox for %s \r\n", parts[1]);
+                    }
+                } else {
+                    // USER not sent, wrong command order
+                    send_formatted(fd, "-ERR need USER name \r\n");
+                }
+                break;
         }
+        read = nb_read_line(nb,recvbuf);
     }
   
     nb_destroy(nb);
