3c5ee1428fce704a5011e12bcb10b28401f8bc7a
lyang13 <lyang13@students.cs.ubc.ca>
2022-03-29 01:30:00 -0700

    SMTP NOOP, VRFY,HELO implemented

--- src/mysmtpd.c
+++ src/mysmtpd.c
@@ -7,75 +7,131 @@
 #include <unistd.h>
 #include <sys/utsname.h>
 #include <ctype.h>
+#include <regex.h>
 
 #define MAX_LINE_LENGTH 1024
+#define WELCOME_SENT_STATE 0
+#define HELO_EHLO_RECEIVED_STATE 1
+#define INVALID_STATE 100
 
 static void handle_client(int fd);
 
-int main(int argc, char *argv[]) {
-  
-    if (argc != 2) {
-	fprintf(stderr, "Invalid arguments. Expected: %s <port>\n", argv[0]);
-	return 1;
+int main(int argc, char *argv[])
+{
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "Invalid arguments. Expected: %s <port>\n", argv[0]);
+        return 1;
     }
-  
+
     run_server(argv[1], handle_client);
-  
+
     return 0;
 }
 
-void handle_client(int fd) {
+void handle_client(int fd)
+{
     char recvbuf[MAX_LINE_LENGTH + 1];
     net_buffer_t nb = nb_create(fd, MAX_LINE_LENGTH);
+    int state;
 
     struct utsname my_uname;
     uname(&my_uname);
 
     // send welcome message
-    char* hostName = my_uname.nodename;
-    if (send_formatted(fd,"220 %s Simple Mail Transfer Service Ready \r\n", hostName) != -1) {
+    char *hostName = my_uname.nodename;
+    if (send_formatted(fd, "220 %s Simple Mail Transfer Service Ready \r\n", hostName) != -1)
+    {
+        state = WELCOME_SENT_STATE;
         dlog("Welcome Message sent sucessfully \n");
-    } else {
-    dlog("Welcome Message sent unsuccesfully \n");
+    }
+    else
+    {
+        dlog("Welcome Message sent unsuccesfully \n");
     }
 
-    // process HELO/EHLO
-    char *parts[MAX_LINE_LENGTH+1];
-    if (recv(fd,recvbuf,MAX_LINE_LENGTH+1,0) != -1) {
-        split(recvbuf,parts);
-    // check if the client sent HELO or EHLO
-        if (strcmp (parts[0], "HELO") == 0) {
-            send_formatted(fd,"250 %s \r\n", hostName);
-        } else if (strcmp (parts[0], "EHLO") == 0) {
-            send_formatted(fd,"250 %s greets %s \r\n", hostName, parts[1]);
+    // read line from socket
+    int read = nb_read_line(nb, recvbuf);
+    while ((read != -1) && (read != 0))
+    {
+        // process HELO/EHLO
+        char *parts[MAX_LINE_LENGTH + 1];
+        split(recvbuf, parts);
+        // generic commands
+        if (strcmp(parts[0], "NOOP") == 0)
+        {
+            send_formatted(fd, "250 OK \r\n");
         }
+        else if (strcmp(parts[0], "QUIT") == 0)
+        {
+            send_formatted(fd, "221 OK \r\n");
+            read = nb_read_line(nb, recvbuf);
+            break;
+            // close connection
+        }
+        else if (strcmp(parts[0], "VRFY") == 0)
+        {
+            if (strchr(parts[1], '@') != NULL)
+            {
+                if (is_valid_user(parts[1], NULL) != 0)
+                {
+                    // username exists
+                    send_formatted(fd, "250 %s \r\n", parts[1]);
+                }
+                else
+                {
+                    send_formatted(fd, "550 no such user - %s\r\n", parts[1]);
+                }
+            } else {
+                send_formatted(fd, "550 no such user - %s\r\n", parts[1]);
+            }
+        }
+        else
+        {
+            // sequential commands
+            switch (state)
+            {
+            // welcome message sent
+            case 0:
+                // check if the client sent HELO or EHLO
+                welcome_state_handler(hostName, parts[0], parts[1], fd, state);
+            }
+        }
+        read = nb_read_line(nb, recvbuf);
     }
-
-    return;
 
     // First step of mail transactions
     // MAIL FROM:<reverse-path> [SP <mail-parameters> ] <CRLF>
     // reset buffer
     // memset(recvbuf,"", MAX_LINE_LENGTH+1);
 
-    // // VRFY
-    // char *parts[MAX_LINE_LENGTH+1];
-    // if (recv(fd,recvbuf,MAX_LINE_LENGTH+1,0) != -1) {
-    //     switch (expression)
-    //     {
-    //     case /* constant-expression */:
-    //         /* code */
-    //         break;
-        
-    //     default:
-    //         break;
-    //     }
-    // }
 
     //  MAIL FROM:<reverse-path> [SP <mail-parameters> ] <CRLF>
-    // take the first <> out , find it 
-    // if okay, return 250 
+    // take the first <> out , find it
+    // if okay, return 250
     // if not, determine if the failure is permanent / temporary  , return with 550/553
     nb_destroy(nb);
+    return;
 }
 
+void welcome_state_handler(char *hostName, char *command, char *content, int fd, int *state)
+{
+    // parts[0] command
+    // parts[1] content
+    if (strcmp(command, "HELO") == 0)
+    {
+        send_formatted(fd, "250 %s \r\n", hostName);
+        state = HELO_EHLO_RECEIVED_STATE;
+    }
+    else if (strcmp(command, "EHLO") == 0)
+    {
+        send_formatted(fd, "250 %s greets %s \r\n", hostName, content);
+        state = HELO_EHLO_RECEIVED_STATE;
+    }
+    else
+    {
+        send_formatted(fd, "503 Bad Sqeuence \r\n");
+        state = INVALID_STATE;
+    }
+}
